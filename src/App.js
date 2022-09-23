6af8d6f1f2ed5241a2a48a51779f9b548a4f9b60
elvinnadir <elvinnadir92@gmail.com>
2022-09-23 15:05:00 +0400

    request-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { usealbums, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [albums, setAlbums] = usealbums([]);
+
+  const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com/'
+
+  });
+
+  const getData = () => {
+    const data = instance.get("/albums")
+      .then((resp) => setAlbums(resp))
+    return data;
+  }
+  console.log("got it",{albums});
+
+  const postData = () => {
+    const data = instance.post("/albums", {
+      method: "POST",
+      body: {
+        title: "hello boys",
+        userId: 1
+      },
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then((resp) => setAlbums(resp.data))
+    return data;
+  };
+  console.log("posted",{albums});
+
+  const removeData = () => {
+    const data = instance.delete("/albums/1")
+      .then((resp) => setAlbums(resp))
+    return data;
+  }
+  console.log("removed",{albums});
+
+  const updateData = () => {
+    const data = instance.put("/albums/1", {
+      userId: '1',
+      body: 'Albums'
+    })
+      .then((resp) => setAlbums(resp))
+    return data;
+  }
+  console.log("updated",{albums});
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getData}>Get Data</button>
+      <button onClick={postData}>Post Data</button>
+      <button onClick={removeData}>Delete Data</button>
+      <button onClick={updateData}>Update Data</button>
+      {albums.map(photo => {
+        return (
+          <div>
+            <h3>{photo.userId}</h3>
+            <h4>{photo.title}</h4>
+          </div>
+        )
+      })
+      }
+
     </div>
   );
 }
